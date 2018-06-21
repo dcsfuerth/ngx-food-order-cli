9838756d79d403e7e73382f1058795ea42078a6b
Marc Remolt <marc.remolt@dcs-fuerth.de>
2018-06-21 13:29:30 +0200

    build(circleci): added circleci config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'dcs';
-
   public ngOnInit() {
     console.log('Site load:', performance.now().toFixed(2), 'ms!!');
   }
